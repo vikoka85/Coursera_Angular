fcef02eafaae53625bfd5873511259492a76ff85
Viktória Juhász-Szalma <126334931+vikoka85@users.noreply.github.com>
2023-10-09 23:36:22 +0200

    New

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,21 +1,41 @@
-(function () {
+(function (){
     'use strict';
     
-    angular.module('MsgApp', [])
+    angular.module('LunchCheck', [])
     .controller('LunchCheckController', LunchCheckController);
     
     LunchCheckController.$inject = ['$scope'];
-    function LunchCheckController($scope) {
-      $scope.name = "Yaakov";
-      $scope.stateOfBeing = "hungry";
+        function LunchCheckController($scope){
+            $scope.name = '';
+            $scope.anotherName = '';
+            //$scope.items = tooMuchQues($scope.name);
     
-      $scope.sayMessage = function () {
-        return "Yaakov likes to eat healthy snacks at night!";
-      };
+            $scope.test = function()  {
+                console.log("hello");
+            };
     
-      $scope.feedYaakov = function () {
-        $scope.stateOfBeing = "fed";
-      };
-    }
+            $scope.buttocks = function() {
+                console.log("hello2");
+            };
+    
+            $scope.tooMuchQues = function(){
+    
+                $scope.i = $scope.name.split(',').length;
+    
+                if($scope.name === '' || $scope.i == 0){
+                    $scope.anotherName = 'Please enter data first'
+                 }
+                else if($scope.i<=3){
+                    $scope.anotherName = 'Enjoy!';
+                }
+    
+                else if($scope.i >3){
+                    $scope.anotherName = 'Too much!';
+                }
+                console.log($scope.i);
+                
+            };
+    
+        }
     
     })();
